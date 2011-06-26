af4180f4487ed771d84c0273bd01089fed30a050
Chris Martello <chrismartello@gmail.com>
2011-06-26 02:31:01 -0500

    * Tables are now implemented as a list of lists. * Tournament.state is eliminated. * Tournament state is now determined by how many active there are.

--- test.py
+++ test.py
@@ -8,26 +8,22 @@ if __name__ == '__main__':
         x = x.strip('\n')
         first, last = x.split(' ')
         event.add_player(Player(first, last))
-    event.generate_pairings()
-    print 'Round ', event.round
-    event.list_tables(showall=True)
 
-    # generate results
-    for x in xrange(4):
-        for x in xrange(len(event.tables)):
-            wins, losses, draws = 0, 0, 0
+    for x in xrange(5):
+        event.start_round()
+        print 'Round: ', event.round
 
-            while (wins+losses+draws) < 3:
-                result = randint(1,100)
-                if result <= 45:
-                    wins += 1
-                elif result >= 55:
-                    losses += 1
-                else:
-                    draws += 1
-
-            event.report_match(x, wins, losses, draws)
-
-        event.generate_pairings()
-        print 'Round :', event.round
         event.list_tables(showall=True)
+
+        for match in event.tables[event.round]:
+            y = randint(1,100)
+            if y < 25:
+                match.report_match(2,1)
+            if y >= 25 and y < 50:
+                match.report_match(2,0)
+            if y >= 50 and y < 75:
+                match.report_match(1,2)
+            if y >= 75 and y < 100:
+                match.report_match(0,2)
+
+        event.finish_round()
